18f69d1b4ac2506935d502f49aa97821b8d5737d
woodb58 <woodb58@gmail.com>
2022-06-20 17:08:18 -0500

    added user routes and auth

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,17 +3,14 @@ const { User } = require('../models');
 const userData = [
     {
         username: "test1",
-        email: "testmail1@gmail.com",
         password: "password123"
     },
     {
         username: "test2",
-        email: "testmail2@gmail.com",
         password: "password123"
     },
     {
         username: "test3",
-        email: "testmail3@gmail.com",
         password: "password123"
     }
    
